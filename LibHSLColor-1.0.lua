180d470b627b206c14614394a2f98f9861488c95
Jason Greer <tuller@tullamods.com>
2018-05-20 15:18:43 -0400

    initial commit

--- LibHSLColor-1.0.lua
+++ LibHSLColor-1.0.lua
@@ -1,6 +1,32 @@
+-- LibHSLColor-1.0
+-- Functions for converting from HSL to RGB and back that mirror the ColorMixin
+-- API
+--
+-- Copyright 2018 Jason Greer
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 local LibHSLColor = _G.LibStub:NewLibrary("LibHSLColor-1.0", 0)
 
-if not LibHSLColor then return end
+if not LibHSLColor then
+    return
+end
 
 local function hueToRGB(m1, m2, h)
     if h < 0 then
@@ -20,7 +46,7 @@ local function hueToRGB(m1, m2, h)
     end
 
     if h * 3 < 2 then
-        return m1 + (m2 - m1) * (2/3 - h) * 6
+        return m1 + (m2 - m1) * (2 / 3 - h) * 6
     end
 
     return m1
@@ -37,9 +63,9 @@ local function hslToRGB(h, s, l)
     end
 
     local m1 = l * 2 - m2
-    local r = hueToRGB(m1, m2, h + 1/3)
+    local r = hueToRGB(m1, m2, h + 1 / 3)
     local g = hueToRGB(m1, m2, h)
-    local b = hueToRGB(m1, m2, h - 1/3)
+    local b = hueToRGB(m1, m2, h - 1 / 3)
 
     return r, g, b
 end
@@ -78,7 +104,7 @@ local function rgbToHSL(r, g, b)
         h = (r - g) / d + 4
     end
 
-    h = h/6
+    h = h / 6
 
     return h, s, l
 end
@@ -90,13 +116,10 @@ local function rgbToHSLA(r, g, b, a)
 end
 
 local HSLColor = {}
-local HSLColor_MT = { __index = HSLColor }
+local HSLColor_MT = {__index = HSLColor}
 
 function HSLColor:IsEqualTo(otherColor)
-	return self.h == otherColor.h
-		and self.s == otherColor.s
-		and self.l == otherColor.l
-		and self.a == otherColor.a
+    return self.h == otherColor.h and self.s == otherColor.s and self.l == otherColor.l and self.a == otherColor.a
 end
 
 function HSLColor:GetHSL()
@@ -116,14 +139,14 @@ function HSLColor:GetRGBA()
 end
 
 function HSLColor:SetHSL(h, s, l)
-	self:SetHSLA(h, s, l, nil)
+    self:SetHSLA(h, s, l, nil)
 end
 
 function HSLColor:SetHSLA(h, s, l, a)
-	self.h = h
-	self.s = s
-	self.l = l
-	self.a = a
+    self.h = h
+    self.s = s
+    self.l = l
+    self.a = a
 end
 
 function HSLColor:SetRGB(r, g, b)
@@ -134,8 +157,8 @@ function HSLColor:SetRGBA(r, g, b, a)
     self:SetHSLA(rgbToHSLA(r, g, b, a))
 end
 
-function LibHSLColor:CreateColor(h, s, l, a)
-    return setmetatable({ h = h, s = s, l = l, a = a }, HSLColor_MT)
+function LibHSLColor.CreateColor(h, s, l, a)
+    return setmetatable({h = h, s = s, l = l, a = a}, HSLColor_MT)
 end
 
 LibHSLColor.RGBToHSL = rgbToHSL
